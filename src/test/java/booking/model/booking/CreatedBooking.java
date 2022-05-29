6b137cc80f3af9496680c0b65bbd6649517b5b63
xoliwar <95bsk@mail.ru>
2022-05-29 10:29:18 +0300

    change model name

--- src/test/java/booking/model/booking/CreatedBooking.java
+++ src/test/java/booking/model/booking/CreatedBooking.java
@@ -3,7 +3,7 @@ package booking.model.booking;
 import lombok.Data;
 
 @Data
-public class NewBooking {
+public class CreatedBooking {
     private Integer bookingid;
     private Booking booking;
 }
